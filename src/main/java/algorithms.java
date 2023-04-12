9adee0132cd09dec37abe942aa55ccdcc0183c0c
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-12 15:31:10 -0700

    First Commit

---
+++ src/main/java/algorithms.java
@@ -0,0 +1,24 @@
+public class algorithms {
+
+
+    public static char[] selectionSort(char[] array){
+        return null;
+    }
+
+
+    public static char[] bubbleSort(char[] array){
+
+
+        return null;
+    }
+
+
+    public static char[] heapSort(char[] array){
+        return null;
+    }
+
+
+    public static char[] quickSort(char[] array){
+        return null;
+    }
+}
