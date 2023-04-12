d001985742c22b0c6c512b72efc9c08ab792605b
Carlos Jimenez Lopez <90943184+carlos-lopez98@users.noreply.github.com>
2023-04-12 15:32:32 -0700

    Adding Comments

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,11 @@
 public class main {
 
-
-
-
-
+    /*
+    *
+    * Selection sort is automatically being run here
+    * Feel free to hop between the different algorithms
+    *
+    * */
     public static void main(String[] args){
 
     }
